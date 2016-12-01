05ca385fb89f99f68e1d5c9f1c77eefbb43d3af1
maulisyeau <maulisyeaudinaulfa@gmail.com>
2016-12-01 12:31:13 +0700

    1Des2016
    chunknya udah rada bener

--- mainForAutoChunk.py
+++ mainForAutoChunk.py
@@ -1,33 +1,68 @@
 __author__ = 'maulisye'
 from preproses import *
+from alignerTA import *
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+import time
+#reload(sys)
+#sys.setdefaultencoding('utf8')
 import  nltk
-#headlinesC ='dataTA/gabung.txt' #udah di chunk yang headline
+from nltk import FreqDist
+start_time=time.time()
+headlineC ='dataTA/gabung.txt' #udah di chunk yang headline
 headlinesC ='dataTA/data/gabungan.txt' #Coba aja dulu hehehe
 #headline ='dataTA/STSint.testinput.headlines.sent2.txt'
 
-# membaca inputan Data chunk
-data = open(headlinesC.lower())
+#Data Chunk
+dataChunk=open((headlineC.lower()))
+bacaChunk=dataChunk.readlines()
+coupleChunk = [] #menyimpan pasangan ayat dalam indeks terpisah
+tokenW1Chunk =[]
+tokenW2Chunk =[]
+jml_lineChunk = sum(1 for line in open(headlineC))
+#tag
+for i in range(0,jml_lineChunk):
+    coupleChunk.append(bacaChunk[i].split('\t'))
+
+    tokenW1Chunk.append(tokenWord((coupleChunk[i][0]).lower()))
+   # print tokenW1
+    tokenW2Chunk.append(tokenWord((coupleChunk[i][1]).lower()))
+  #  print tokenW2
+
+
+    #removal tagnya
+    for j in range(len(tokenW1Chunk[i])):
+        tokenW1Chunk[i][j]=Removal(tokenW1Chunk[i][j])
+        tokenW1Chunk[i][j]=Hapus_stopword(tokenW1Chunk[i][j])
+    for j in range(len(tokenW2Chunk[i])):
+        tokenW2Chunk[i][j]=Removal(tokenW2Chunk[i][j])
+        tokenW2Chunk[i][j]=Hapus_stopword(tokenW2Chunk[i][j])
+
+
+
+#Hapus spasi token
+    j=0
+    while j < len(tokenW1Chunk[i]) :
+        if tokenW1Chunk[i][j]=='':
+            tokenW1Chunk[i].pop(j)
+        else:
+            j +=1
+
+    j = 0
+    while j < len(tokenW2Chunk[i]):
+        if tokenW2Chunk[i][j] == '':
+            tokenW2Chunk[i].pop(j)
+        else:
+            j += 1
+
+
+
+# membaca inputan Data  Bukan chunk
+data = open((headlinesC).lower())
 baca = data.readlines()
+
 #hitung jumlah baris data
 jml_line = sum(1 for line in open(headlinesC))
 print  "Jumlah baris :",jml_line
-
-
-
-
-# r =[]
-# text = nltk.word_tokenize(" Conservatives sweep to Australia election victory ")
-# print  nltk.pos_tag(text)
-# sentence = nltk.pos_tag(text)
-# grammar = "NP:{<DT>?<JJ>*<NN>}"
-# cp = nltk.RegexpParser(grammar)
-# result = cp.parse(sentence)
-# r.append(result)
-# print result.draw()
-# print r
 
 i= 0
 
@@ -37,15 +72,20 @@ tokenW2 =[]
 potonganP1 =[]
 potonganP2 =[]
 
+k=0
+Chunk1=[]
+Chunk2=[]
+HasilChunkFIX1=[]
+HasilChunkFIX2=[]
 #PREPROSES#
 
-#tag
+#tag bukan chunk
 for i in range(0,jml_line):
     couple.append(baca[i].split('\t'))
 
-    tokenW1.append(tokenWord(couple[i][0]))
+    tokenW1.append(tokenWord((couple[i][0]).lower()))
    # print tokenW1
-    tokenW2.append(tokenWord(couple[i][1]))
+    tokenW2.append(tokenWord((couple[i][1]).lower()))
   #  print tokenW2
 
     potonganP1.append(tokenFrase(couple[i][0]))
@@ -98,33 +138,56 @@ for i in range(0,jml_line):
         else:
             j += 1
 
-k=0
-HasilChunk1=[]
-HasilChunk2=[]
-def removeNoPar(token):
-    # menghapus semua tanda
-    token = re.sub(r'([A-Z])\w+', '', str(token))
-    token = re.sub(r'[,"-$-\']', '', str(token))  # menghapus tanda hubung , ' . ;
-    token = re.sub('\n', '', str(token))
-    return token
+
+
+
+align_word=[]
+word1=[]
+word2=[]
+identik=[]
+ss=[]
+s_identic=[]
+sim_identic=[]
+align1=[]
+align2=[]
+konteksKata =[]
+alignIdentic=0
+
+
+aligned_word1PPDB=[]
+aligned_word2PPDB=[]
+hasilPFA=[]
+
+loadPPDB(ppdbNamaFile='dataTA/penting/ppdb-2.0-xxxl-lexical')
+#loadPPDB(ppdbNamaFile='dataTA/penting/ppdb-1.0-xxxl-lexical.extended.synonyms.uniquepairs')
+align_ppdb (jml_line,aligned_word1PPDB,aligned_word2PPDB,tokenW1,tokenW2)
+
 for k in range(0,jml_line):
      print ("==========================================================================================================================")
      print ("==========================================================================================================================")
      print ("Pasangan ayat Al-qur'an Ke- ",k+1)
-     print (couple[k][0])
-     print (couple[k][1])
+     print "ayat 1 = " ,couple[k][0]
+     print "ayat 2 = ",couple[k][1]
      r1 = []
      r2 = []
-     text = nltk.word_tokenize((couple[k][0]).lower())
-     text2 = nltk.word_tokenize((couple[k][1]).lower())
+     text1 = nltk.word_tokenize(Hapus_stopword((couple[k][0]).lower()))
+     text2 = nltk.word_tokenize(Hapus_stopword((couple[k][1]).lower()))
+     #print "text 1:",text1
 
-
-     sentence1 = nltk.pos_tag(text)
+     sentence1 = nltk.pos_tag(text1)
      sentence2 = nltk.pos_tag(text2)
      # grammar = "NP:{<DT>?<JJ>*<NN>}"
      grammar = r"""
-     Chunk:{<PP>*<NP>},
-     Chunk:{<VP>*<PRT>},
+     Chunk:{<IN>}
+     Chunk:{<RB>}
+     Chunk:{<V.*>}
+     Chunk:{<P> <NP>}
+     Chunk:{<NP>?<CC>*<NP>}
+     Chunk:{<PP>*<NP>}
+     Chunk:{<VP>*<PRT>}
+     Chunk:{<V> <NP|PP>*}
+     Chunk:{<PP>*<NP>}
+     Chunk:{<PRP>*<VP>*<PRT>}
      Chunk:{<DT>?<JJ>*<NN>}
      """
      cp = nltk.RegexpParser(grammar)
@@ -136,74 +199,108 @@ for k in range(0,jml_line):
 
 
      print (" Pasangan ayat Al-quran yang sudah di chunk :")
+     #nampilin parsernya
      print "result1",result1
-     print result2
-     # print "Hasil chunk 1:",HasilChunk1
-     # print "Hasil chunk 2:",HasilChunk2
-     # print result1.draw()
-     # print result2.draw()
+     print "result2",result2
      jml=0
      HasilChunk11=[]
      HasilChunk22=[]
      for subtree1 in result1.subtrees(filter=lambda t: t.label() == 'Chunk'):
-         print ("hasil subtree :", subtree1)
+         print "hasil subtree 1:", subtree1
          HasilChunk11.append(subtree1)
 
      for subtree2 in result2.subtrees(filter=lambda t: t.label() == 'Chunk'):
-         print ("hasil subtree :", subtree2)
+         print "hasil subtree 2:", subtree2
          HasilChunk22.append(subtree2)
 
-     HasilChunk1.append(HasilChunk11)
-     HasilChunk2.append(HasilChunk22)
-     print "chunk 1 ya", HasilChunk1[k]
-     print "chunk 2 ya", HasilChunk2[k]
-     print "Jumalah chunk 1 di potongan ayat 1:",len(HasilChunk1[k])
+     Chunk1.append(HasilChunk11)
+     Chunk2.append(HasilChunk22)
+     print "-------------------------------------------"
+     print "chunk 1 ya", Chunk1[k]
+     print "chunk 2 ya", Chunk2[k]
+     jmlc1=len(Chunk1[k])
+     jmlc2 = len(Chunk2[k])
+     jmlk1=len(set(FreqDist(text1)))
+     jmlk2 = len(set(FreqDist(text2)))
+     print "Jumlah chunk di potongan ayat 1=",jmlc1,"jumlah kata di potongan ayat 1 =",jmlk1 ,"rata-rata =",float (jmlc1)/(jmlk1)
+     print "Jumlah chunk di potongan ayat 2=", jmlc2, "jumlah kata di potongan ayat 1 =", jmlk2, "rata-rata =", float (jmlc2)/(jmlk2)
+     print "==================================================="
+     #print "removal:",removeNoChunk(HasilChunk11)
+     #print "removal:",removeNoChunk(HasilChunk22)
+     HasilChunkFIX1.append(removeNoChunk(HasilChunk11))
+     HasilChunkFIX2.append(removeNoChunk(HasilChunk22))
+     print "Hasil chunk 1 fix nih = ",HasilChunkFIX1[k]
+     print "Hasil chunk 2 fix nih = ", HasilChunkFIX2[k]
      print (" Hasil Proses tokenisasi : ")
      print (tokenW1[k])
      print (tokenW2[k])
+
+     # Lemmatization
+     for y in range(0, jml_line):
+         for tes1 in range(len(tokenW1[y])):
+             #t = unicode(tokenW1[y][tes1], errors='replace')
+             tokenW1[y][tes1] = lematisasi(tokenW1[y][tes1])
+             # print t
+             # print  tokenW1[y][tes1]
+
+         for tes2 in range(len(tokenW2[y])):
+             #ts2 = unicode(tokenW2[y][tes2], errors='replace')
+             tokenW2[y][tes2] = lematisasi(tokenW2[y][tes2])
+             # print t
+             # tokenW2[y][tes2]=lematisasi((tokenW2[y][tes2]).lower())
+             # print  tokenW2[y][tes2]
+
+     print "========================================="
+     print (" Hasil Proses  Stemming dan lemmatisasi :")
+     print tokenW1[k]
+     print tokenW2[k]
+
+
+     a = open('hasil/identik.txt', 'w')
+     alignIdenticalWord(jml_line, align1, align2, tokenW1, tokenW2)
+
+     print "============================"
+     print "=====Identical Alignment==== :"
+     print align1[k]
+     print align2[k]
+
      #
+
+     # f.writelines(s_identic)
+     # f.close()
+     # g.writelines(sim_identic)
+     # g.close()
+
+     # Alignment dengan ppdb /pharaprase
+
+
+     print "================================="
+     print "======Alignment dengan PPDB======"
+     print aligned_word1PPDB[4]
+     print aligned_word2PPDB[4]
+
      k +=1
 
 
-for hsl in HasilChunk1:
-    print "Sementara 1",str(hsl)+"\n"
 
 
-# print ("Pasangan ayat Al-qur'an : ")
-# c1= couple[4][0]
-# c2=couple[4][1]
-# print (c1)
-# print (c2)
-
-
-#print result1.draw()
-#print  r1
-
-#print result2.draw()
-#print  r2
+# for hsl in HasilChunk1:
+#     print "Sementara 1",str(hsl)+"\n"
 
 
 
-# l=0
-# for l in range(0,jml_line):
-#     print (" Hasil Proses tokenisasi : ",l+1)
-#     print (tokenW1[l])
-#     print (tokenW2[l])
+# NE
+
+aligned_word1NE=[]
+aligned_word2NE=[]
+hasilNE=[]
+
+LoadNE(neNamaFile='dataTA/penting/ne.txt')
+align_NE(jml_line,aligned_word1NE,aligned_word2NE,tokenW1,tokenW2)
+print "=============================="
+print "========Named entity=========="
+print aligned_word1NE[4]
+print aligned_word2NE[4]
 
 
-#Lemmatization
-for y in range(0,jml_line):
-    for tes1 in range(len(tokenW1[y])):
-        t = unicode(tokenW1[y][tes1], errors='replace')
-        tokenW1[y][tes1]=prosesStemdanLem(t)
-        #print t
-        #print  tokenW1[y][tes1]
-
-    for tes2 in range(len(tokenW2[y])):
-
-        ts2 = unicode(tokenW2[y][tes2], errors='replace')
-        tokenW2[y][tes2] = prosesStemdanLem(ts2)
-        #print t
-        #tokenW2[y][tes2]=lematisasi((tokenW2[y][tes2]).lower())
-        #print  tokenW2[y][tes2]
-
+print ("---%s seconds---"% (time.time()-start_time))
